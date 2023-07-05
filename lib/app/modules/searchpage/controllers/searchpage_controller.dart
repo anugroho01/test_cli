76e686a0dc37faa7b15ff3d2893c04c42fbdd1f8
anugroho01 <nugrohoariska@gmail.com>
2023-07-05 21:35:29 +0700

    searching

--- lib/app/modules/searchpage/controllers/searchpage_controller.dart
+++ lib/app/modules/searchpage/controllers/searchpage_controller.dart
@@ -1,9 +1,61 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SearchpageController extends GetxController {
   //TODO: Implement SearchpageController
   late TextEditingController searchC;
+
+  var awalan = [].obs;
+  var tempSearch = [].obs;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void cariOrang(String data) async {
+    //print("Cari $data");
+
+    if (data.length == 0) {
+      awalan.value = [];
+      tempSearch.value = [];
+    } else {
+      var kapital = data.substring(0, 1).toUpperCase() + data.substring(1);
+      print(kapital);
+
+      if (awalan.length == 0 && data.length == 1) {
+        //jalan saat ketik huruf pertama
+        CollectionReference karyawan = await firestore.collection("personels");
+        final KarResult = await karyawan
+            .where("KeyName", isEqualTo: data.substring(0, 1).toUpperCase())
+            .get();
+
+        print(KarResult.docs.length);
+
+        if (KarResult.docs.length > 0) {
+          for (var i = 0; i < KarResult.docs.length; i++) {
+            awalan.add(KarResult.docs[i].data() as Map<String, dynamic>);
+          }
+          print("result");
+          print(awalan);
+        } else {
+          print("no data");
+        }
+      }
+
+      if (awalan.length != 0) {
+        tempSearch.value = [];
+        awalan.forEach((element) {
+          if (element["name"].startsWith(kapital)) {
+            tempSearch.add(element);
+          }
+        });
+      }
+    }
+    awalan.refresh();
+    tempSearch.refresh();
+  }
 
   @override
   void onInit() {
--- lib/app/modules/searchpage/views/searchpage_view.dart
+++ lib/app/modules/searchpage/views/searchpage_view.dart
@@ -8,76 +8,75 @@ import '../controllers/searchpage_controller.dart';
 
 class SearchpageView extends GetView<SearchpageController> {
   SearchpageView({Key? key}) : super(key: key);
-  final List<Widget> temans = List.generate(
-    11,
-    (index) => ListTile(
-      onTap: () => Get.toNamed(Routes.CHATROOM),
-      leading: CircleAvatar(
-        radius: 25,
-        backgroundColor: Colors.grey,
-        child: Image.asset(
-          "assets/logo/noimage.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        "nama ke ${index + 1}",
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      subtitle: Text(
-        "email${index + 1}@gmal.com",
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      trailing: Chip(label: Text("Message")),
-    ),
-  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          child: AppBar(
-            title: const Text('Search'),
-            backgroundColor: Colors.red,
-            centerTitle: true,
-            flexibleSpace: Padding(
-                padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: TextField(
-                    controller: controller.searchC,
-                    cursorColor: Colors.red,
-                    decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        hintText: "Search Friend",
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                        suffixIcon: InkWell(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Icon(
-                            Icons.search_rounded,
-                            color: Colors.red,
-                          ),
-                          onTap: () {},
-                        )),
+        appBar: PreferredSize(
+            child: AppBar(
+              title: const Text('Search'),
+              backgroundColor: Colors.red,
+              centerTitle: true,
+              flexibleSpace: Padding(
+                  padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: TextField(
+                      onChanged: (value) => controller.cariOrang(value),
+                      controller: controller.searchC,
+                      cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          hintText: "Search Friend",
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 20),
+                          suffixIcon: InkWell(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Icon(
+                              Icons.search_rounded,
+                              color: Colors.red,
+                            ),
+                            onTap: () {},
+                          )),
+                    ),
+                  )),
+            ),
+            preferredSize: Size.fromHeight(125)),
+        body: Obx(
+          () => controller.tempSearch.length == 0
+              ? Center(
+                  child: Container(
+                    height: Get.width * 0.8,
+                    width: Get.width * 0.8,
+                    child: Lottie.asset("assets/lottie/empty.json"),
                   ),
-                )),
-          ),
-          preferredSize: Size.fromHeight(125)),
-      body: temans.length == 0
-          ? Center(
-              child: Container(
-                height: Get.width * 0.8,
-                width: Get.width * 0.8,
-                child: Lottie.asset("assets/lottie/empty.json"),
-              ),
-            )
-          : ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: temans.length,
-              itemBuilder: (contrx, index) => temans[index]),
-    );
+                )
+              : ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount: controller.tempSearch.length,
+                  itemBuilder: (contex, index) => ListTile(
+                        onTap: () => Get.toNamed(Routes.CHATROOM),
+                        leading: CircleAvatar(
+                          radius: 25,
+                          backgroundColor: Colors.grey,
+                          child: Image.asset(
+                            "assets/logo/noimage.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        title: Text(
+                          "${controller.tempSearch[index]["name"]}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          "${controller.tempSearch[index]["email"]}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Chip(label: Text("Message")),
+                      )),
+        ));
   }
 }
